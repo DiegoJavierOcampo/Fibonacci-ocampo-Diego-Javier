fb21e997c7c184da4fc5831df25dde66f7f73481
Diego Javier Ocampo <hipokampo69@hotmail.com>
2022-11-11 22:16:34 -0300

    first commit

---
+++ data/datos.js
@@ -0,0 +1,22 @@
+const usersData = [
+    {
+        id: 1,
+        nombre: "Marcelo",
+        apellido: "Gallardo",
+        dni: 23009312,
+    },
+    {
+        id: 2,
+        nombre: "Lionel",
+        apellido: "Messi",
+        dni: 33805421,
+    },
+    {
+        id: 3,
+        nombre: "Adabel",
+        apellido: "Rodriguez",
+        dni: 35162904,
+    },
+];
+
+module.exports = usersData
